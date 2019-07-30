79c25a59514fa7165134370daee591298e7c9efd
Olya Wysocky <OlyaW@provokesolutions.com>
2019-07-29 20:06:17 -0700

    add arrow function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Allo TypeScript!');
+const pizzas = [{ name: "Cheese", toppings: ["cheese"] }];
+
+const mappedPizzas = pizzas.map(function(pizza) {
+  return pizza.name;
+});
+
+const mappedPizzas2 = pizzas.map(pizza => pizza.name);
+
+const pizza = {
+  name: "This NAME",
+  getName: function() {
+    setTimeout(() => {
+      console.log(this.name);
+    }, 100);
+  }
+};
+
+console.log(mappedPizzas);
